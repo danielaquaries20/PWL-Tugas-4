5b588e2507c1dbd40d57688312bd4e5594816bca
danielaquaries20 <danielaquaries@gmail.com>
2025-04-09 22:00:45 +0700

    Latihan 6 - Session

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,8 +10,12 @@ $routes->get('/home', 'Home::index');
 $routes->get('/produk', 'Home::produk');
 $routes->get('/keranjang', 'Home::keranjang');
 
-$routes->get('/home/about', 'Home::about');
-$routes->get('/home/services', 'Home::services');
-$routes->get('/home/contact', 'Home::contact');
-$routes->get('/home/description', 'Home::description');
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+// $routes->get('/home/about', 'Home::about');
+// $routes->get('/home/services', 'Home::services');
+// $routes->get('/home/contact', 'Home::contact');
+// $routes->get('/home/description', 'Home::description');
 
